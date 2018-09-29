d15998dd4869073a57ff2857792b20208a719c48
Bogdan Roatis <aetheern@gmail.com>
2018-09-29 11:43:50 +0300

    Initial commit

---
+++ lib/src/main/java/bogdan/roatis/lib/Extensions.kt
@@ -0,0 +1,19 @@
+package bogdan.roatis.lib
+
+import android.text.StaticLayout
+
+/**
+ *
+ * Created by Bogdan Roatis on 29/09/2018.
+ */
+
+/**
+ * Calculated the widest line in a [StaticLayout].
+ */
+fun StaticLayout.textWidth(): Int {
+    var width = 0f
+    for (i in 0 until lineCount) {
+        width = width.coerceAtLeast(getLineWidth(i))
+    }
+    return width.toInt()
+}
